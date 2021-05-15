242d9ac92517be8940171b136e8e890f2c33cc8b
sharopcha <napster1202@gmail.com>
2021-05-15 19:49:17 +0300

    change for puria

---
+++ auto-text-effect/readme.md
@@ -0,0 +1,6 @@
+Auto text effect
+
+1. Functionalities
+
+   Create simple typing effect.
+   Change the typing speed based on given value
--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,22 +1,30 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
+// Get all divs that we need to work
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
 
-writeText()
+// text to create typing effect
+const text = 'Kalat Zadeh Abolfazl';
+// slicing value. We need it to slice the letters to create the effect
+let idx = 1;
 
+// speed value.
+let speed = 300 / speedEl.value;
+
+writeText();
+
+// function to create text and set it to innerText.
+// function compares the length of the given text and appends it to textEl with a given time interval
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
+  textEl.innerText = text.slice(0, idx);
 
-    idx++
+  idx++;
 
-    if(idx > text.length) {
-        idx = 1
-    }
+  if (idx > text.length) {
+    idx = 1;
+  }
 
-    setTimeout(writeText, speed)
+  setTimeout(writeText, speed);
 }
 
-
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+// event listener to set speed value
+speedEl.addEventListener('input', (e) => (speed = 300 / e.target.value));
